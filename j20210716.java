e3fa72f9962dec40370fc79261df01196522d4c0
eason1987128 <91937272+eason1987128@users.noreply.github.com>
2021-10-05 16:16:35 +0800

    Create j20210716.java

---
+++ j20210716.java
@@ -0,0 +1,9 @@
+package J20210716;
+
+public class j20210716 {
+
+	public static void main(String[] args) {
+        // TODO Auto-generated method stub
+	   System.out.print("這是我第一支JAVA程式");	
+	 }	 
+}
